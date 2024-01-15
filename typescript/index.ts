e14e983f96a97f42c7a360f0d4809bae0691aa15
youngeunkim <pkhj286@gmail.com>
2024-01-15 10:17:13 +0900

    Add : 내가 한 방법

---
+++ typescript/index.ts
@@ -0,0 +1,11 @@
+let 이름: string = "kim";
+let 나이: number = 18;
+let 출생지역: string = "부산";
+
+let obj: { singer: string; song: string } = { singer: "태연", song: "to X" };
+
+let project: { member: string[]; days: number; started: boolean } = {
+  member: ["kim", "park"],
+  days: 30,
+  started: true,
+};
